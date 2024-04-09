e78eeb2dd10ea381d3b208fd96e5cc7f9cffb874
Vidit Khazanchi <92437375+vk0812@users.noreply.github.com>
2024-04-10 03:43:52 +0530

    Update config.py
    Updated Attack Technique

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # settings
 OPENAI_API_KEY = "" # Insert Key Here
 API_KEY = "dfa71d624d70486cb70131629230908" # Insert Key Here
-GEMINI_API_KEY = "AIzaSyD3XV5dOE9NUwK30IKsEf-Rry6TG_kUCIc" # Insert Key Here
+GEMINI_API_KEY = "" # Insert Key Here
 API_HOST = "api-football-v1.p.rapidapi.com"
 API_URL = "http://api.weatherapi.com/v1/current.json"
 QUERYSTRING = {
